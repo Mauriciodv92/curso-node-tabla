5f78647f9b6b5d6e846f18d0e31408f92fd865c5
Mauricio Díaz <mauriciodv92@hotmail.com>
2023-08-18 15:33:11 -0500

    Commit correcto

--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -1,36 +1,37 @@
-const { rejects } = require('assert');
 const fs = require('fs');
+const colors = require('colors');
 
+const crearArchivo = async( base = 5, listar = false, hasta = 10 ) => {
 
-const crearArchivo = async(base = 5, listar = false) =>{
+    try {
 
-    try{
+        let salida  = '';
+        let consola = '';
+        
+        for( let i = 1; i <= hasta; i++ ) {
+            salida  += `${ base } x ${ i } = ${ base * i }\n`;
+            consola += `${ base } ${ 'x'.green } ${ i } ${ '='.green } ${ base * i }\n`;
+        }
 
+        if ( listar ) {
+            console.log('===================='.green);
+            console.log('   Tabla del:'.green, colors.blue( base ) );
+            console.log('===================='.green);
+
+            console.log(consola);
+        }
         
 
-        let salida = '';
+        fs.writeFileSync( `./salida/tabla-${ base }.txt`, salida );
 
-        for(let i = 1; i <= 10; i++){
-            salida +=`${base} x ${i} = ${base*i}\n`;
-        }
 
-        if(listar){
-
-            console.log('=======================');
-            console.log('   Tabla del:', base);
-            console.log('=======================');
-            console.log(salida);
-        }
-
-        fs.writeFileSync(`tabla-${base}.txt`, salida);
-
-        return`tabla-${base}.txt`;
-    }catch(error){
+        return `tabla-${ base }.txt`;
+        
+    } catch (err) {
         throw err;
-
     }
-        
-        
+
+
 
 }
 
