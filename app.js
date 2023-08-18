b8afef7e2a74d27e8ee415194c118b1e6ccba4df
Mauricio Díaz <mauriciodv92@hotmail.com>
2023-08-18 15:34:38 -0500

    Ultimo commit

--- app.js
+++ app.js
@@ -1,48 +1,11 @@
+const { crearArchivo } = require('./helpers/multiplicar');
+const argv = require('./config/yargs');
 
-const { log } = require('console');
-const {crearArchivo} = require('./helpers/multiplicar');
-const { option } = require('yargs');
-
-
-const argv = require('yargs')
-                .option('b',{
-                    alias:'base' ,
-                    type:'number',
-                    demandOption:true
-                })
-                .option('l',{
-                    alias:'listar' ,
-                    type:'boolean',
-                    demandOption:true,
-                    default:false
-                })
-                .check((argv, options) =>{
-                    if(isNaN(argv.b)){
-                        throw'La base tiene que ser un número'
-                    }
-                    return true;
-                })
-                .argv;
-
-
-
-
+require('colors');
 
 
 console.clear();
 
-
-
-console.log( argv );
-
-
-
-// const base = 3;
-
-
- crearArchivo(argv.b, argv.l)
-     .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
-     .catch(err => console.log(err));
-
-
-
+crearArchivo( argv.b, argv.l, argv.h )
+    .then( nombreArchivo => console.log(nombreArchivo.rainbow, 'creado') )
+    .catch( err => console.log(err) );
